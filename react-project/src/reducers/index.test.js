06f6b94f99cccac027ff0b6dff7ec0160038011b
Troy Lamerton <troy.lamerton@gmail.com>
2017-08-07 10:44:22 +0200

    remove product test passing, begin update quantity test

--- react-project/src/reducers/index.test.js
+++ react-project/src/reducers/index.test.js
@@ -12,13 +12,34 @@ test('store is unchanged by empty action', () => {
   expect(state1).toBe(state2);
 });
 
+function addRandomProductToState (state) {
+  const productId = chooseRandomKey(db);
+  const stateAfterAdd = reducer(state, actions.addProduct(productId));
+  return {
+    stateAfterAdd,
+    productId
+  };
+}
+
 test('add product creates new meta and product', () => {
   const state1 = reducer(initialState);
-  const productId = chooseRandomKey(db);
-  const stateAfterAdd = reducer(state1, actions.addProduct(productId));
+  const { stateAfterAdd, productId } = addRandomProductToState(state1);
   const { products, meta } = stateAfterAdd.cart;
   expect(_.size(products)).toBe(1);
   expect(_.size(meta)).toBe(1);
   expect(products).toHaveProperty(productId);
   expect(meta).toHaveProperty(productId);
+});
+
+test('removed products are not in state', () => {
+  const state1 = reducer(initialState);
+  const { stateAfterAdd, productId } = addRandomProductToState(state1);
+  const stateAfterRemove = reducer(stateAfterAdd, actions.removeProduct(productId));
+  const { products, meta } = stateAfterRemove.cart;
+  expect(products).not.toHaveProperty(productId);
+  expect(meta).not.toHaveProperty(productId);
+});
+
+test('can update quantity without changing product', () => {
+  expect('todo').toBe('done');
 });
